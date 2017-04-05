83645fa56a6ef57e682578071e482330a80dd629
hexian000 <he.xian@163.com>
2017-04-05 16:08:08 +0800

    add assembly description

--- FileHistoryStandalone/Properties/AssemblyInfo.cs
+++ FileHistoryStandalone/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("File History Standalone")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/hexian000/FileHistoryStandalone")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("File History Standalone")]
